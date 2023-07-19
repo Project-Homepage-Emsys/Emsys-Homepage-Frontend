18e73f55d0a593ae0acc12d72cee25dd961eaa11
LikeNoru <stkjas4657@naver.com>
2023-07-20 01:02:28 +0900

    Board

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Nav from './Nav/Nav';
 import Main from './Main';
 //지연 작업공간 
 import Footer from './Footer/Footer';
+import Community from './Community/Community';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +15,7 @@ root.render(
     <Main/>
     <Nav/>
     <Footer/>
+    <Community/>
   </React.StrictMode>
 );
 
